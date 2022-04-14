a315e1e21947712acd51080c346514d3bb3fd0a6
benjaminlangenaken <benjamin.langenaken@outlook.com>
2022-04-14 12:27:03 +0200

    Add component for selecting all todos as completed

---
+++ src/components/CheckAllTodos.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CheckAllTodos = (props) => {
+	return (
+		<div className="check-all-container">
+			<div>
+				<div onClick={props.completeAllTodos} className="button">
+					Check all
+				</div>
+			</div>
+			<span>{props.remainingTodos()} items remaining</span>
+		</div>
+	);
+};
+
+CheckAllTodos.propTypes = {
+	remainingTodos: PropTypes.func.isRequired,
+	completeAllTodos: PropTypes.func.isRequired,
+};
+
+export default CheckAllTodos;
